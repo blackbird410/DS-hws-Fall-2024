d0b45df4d99a22b1f3c4b5521a857c2ecae70fd6
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-01-01 15:48:13 +0800

    add hw4 files

---
+++ hw4/hw4_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct Edge {
+  int s, e;
+};
+
+int main() {
+  int n, m, i, vertices *;
+  std::cin >> n >> m;
+
+  vertices = new int[n];
+  for (i = 0; i < n; i++)
+    std::cin >> vertices[i];
+};
