4dde4f806e17b7cfa833afe0e2e6c31c03f7076c
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2025-01-05 20:26:37 +0800

    add: hw4_b solution

--- hw4/hw4_b.cpp
+++ hw4/hw4_b.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <climits>
 #include <fstream>
 #include <iostream>
 #include <queue>
@@ -7,6 +8,7 @@
 #include <unordered_map>
 #include <vector>
 
+inline void print(std::string s) { std::cout << s << std::endl; }
 
 template <class T> class Node {
 public:
@@ -38,10 +40,17 @@ private:
 template <class T> class NodeList {
 public:
   NodeList() : head(nullptr), tail(nullptr), count(0) {};
+  NodeList(const NodeList<T> *other) {
+    Node<T> *p = other->getHead();
+    while (p) {
+      this->insert(p->getData());
+      p = p->getNext();
+    }
+  };
   ~NodeList() { deleteNode(head); };
 
   Node<T> *operator[](int i) const {
-    if (i > getSize() || i < getSize())
+    if (i >= getSize() || i < 0)
       return nullptr;
 
     Node<T> *p = getHead();
@@ -50,6 +59,7 @@ public:
       if (i == j)
         return p;
       j++;
+      p = p->getNext();
     }
     return nullptr;
   };
@@ -59,11 +69,11 @@ public:
 
     int i = 0;
     while (p) {
-      if constexpr (std::is_pointer<T>::value)
+      if constexpr (std::is_pointer<T>::value) {
         if (*(p->getData()) == *d)
           return i;
-        else if (p->getData() == d)
-          return i;
+      } else if (p->getData() == d)
+        return i;
       i++;
       p = p->getNext();
     }
@@ -76,11 +86,11 @@ public:
     Node<T> *p = head;
 
     while (p) {
-      if constexpr (std::is_pointer<T>::value)
+      if constexpr (std::is_pointer<T>::value) {
         if (*(p->getData()) == *d)
           return p;
-        else if (p->getData() == d)
-          return p;
+      } else if (p->getData() == d)
+        return p;
 
       p = p->getNext();
     }
@@ -89,23 +99,25 @@ public:
   }
 
   void removeFromHead() {
-    if (!head) return;
+    if (!head)
+      return;
 
-    Node<T>* tmp = head;
+    Node<T> *tmp = head;
     tail = (tail == head) ? nullptr : tail;
     head = head->getNext();
     delete tmp;
   };
 
   void removeFromTail() {
-    if (!head) return;
+    if (!head)
+      return;
     if (head == tail) {
       delete head;
       head = tail = nullptr;
       return;
     };
 
-    Node<T>* tmp = head;
+    Node<T> *tmp = head;
 
     while (tmp) {
       if (tmp->getNext() == tail) {
@@ -118,6 +130,14 @@ public:
     }
   };
 
+  void addFromHead(const T &data) {
+    Node<T> *newNode = new Node<T>(data);
+    newNode->setNext(head);
+    tail = (!tail) ? newNode : tail;
+    head = newNode;
+  };
+
+  void addFromTail(const T &data) { insert(data); };
   void insert(const T &data) {
     if (exists(data))
       return;
@@ -196,7 +216,7 @@ private:
 
 class Edge {
 public:
-  Edge(Vertex *u, Vertex *v, int w = 1) : vertices({u, v}), weight(w) {};
+  Edge(Vertex *u, Vertex *v, int w = 1) : weight(w), vertices({u, v}) {};
   ~Edge() {};
   bool operator==(const Edge *other) const {
     return (getWeight() == other->getWeight() &&
@@ -216,10 +236,12 @@ public:
 
   bool operator!=(const Edge &other) const { return !(*this == other); };
 
+  Vertex *operator[](const int &i) const {
+    return (i != 0 && i != 1) ? nullptr : vertices[i];
+  };
+
   Vertex *getAnotherEnd(const Vertex *v) const {
-    // Return the second edge only when trying to access the vertices because of
-    // directed edge property
-    return (v == vertices[0]) ? vertices[1] : nullptr;
+    return (v == vertices[0]) ? vertices[1] : vertices[0];
   };
 
   int getWeight() const { return weight; };
@@ -257,7 +279,7 @@ public:
     delete edgeList;
   }
 
-  void insertVertex(int &d) {
+  void insertVertex(int d) {
     Vertex *v = new Vertex(d);
     if (!vertexList->exists(v)) {
       vertexList->insert(v);
@@ -265,7 +287,7 @@ public:
       delete v;
   };
 
-  void insertEdge(const int &u, const int &v, const int& w = 1) {
+  void insertEdge(const int &u, const int &v, const int &w = 1) {
     Vertex v1(u), v2(v);
     Vertex *uExists = vertexList->exists(&v1)->getData();
     Vertex *vExists = vertexList->exists(&v2)->getData();
@@ -302,6 +324,45 @@ public:
     }
   };
 
+  void printAdjacentMatrix(int ***adjMat = nullptr) const {
+    int n = vertexList->getSize(), u, v;
+    int **adjMatrix = (adjMat) ? *adjMat : nullptr;
+    bool freeMemory = false;
+    print("Adjacent matrix: ");
+
+    if (!adjMatrix) {
+      getAdjacentMatrix(adjMatrix);
+      freeMemory = true;
+    }
+
+    std::cout << "\t| ";
+    for (u = 0; u < n; u++, std::cout << "\t")
+      std::cout << (*vertexList)[u];
+    std::cout << std::endl;
+    std::cout << "\t|";
+    for (u = 0; u < 7 * (n + 1); u++, std::cout << "_")
+      ;
+    std::cout << std::endl;
+
+    for (u = 0; u < n; u++) {
+      std::cout << (*vertexList)[u] << "\t| ";
+      for (v = 0; v < n; v++, std::cout << "\t") {
+        if (adjMatrix[u][v] != INT_MAX)
+          std::cout << adjMatrix[u][v];
+        else
+          std::cout << "-";
+      }
+      std::cout << std::endl;
+    }
+
+    // Clean up
+    if (freeMemory) {
+      for (u = 0; u < n; u++)
+        delete[] adjMatrix[u];
+      delete[] adjMatrix;
+    }
+  };
+
   void getAdjacentMatrix(int **&adjMatrix) const {
     // Get the number of vertices
     int dim = vertexList->getSize();
@@ -313,10 +374,10 @@ public:
     for (int i = 0; i < dim; i++)
       adjMatrix[i] = new int[dim];
 
-    // Initialize the matrix elements to 0
+    // Initialize the matrix elements to highest possible value
     for (int i = 0; i < dim; i++)
       for (int j = 0; j < dim; j++)
-        adjMatrix[i][j] = 0;
+        adjMatrix[i][j] = INT_MAX;
 
     // Populate the adjacency matrix
     Node<Vertex *> *p = vertexList->getHead();
@@ -341,18 +402,180 @@ public:
     }
   }
 
+  Edge *getEdge(const int &u, const int &v, const int &w) const {
+    Node<Edge *> *e = edgeList->getHead();
+    while (e) {
+      if ((*e->getData())[0]->getData() == u &&
+          (*e->getData())[1]->getData() == v && e->getData()->getWeight() == w)
+        return e->getData();
+      e = e->getNext();
+    }
+    return nullptr;
+  };
+
+  NodeList<Vertex *> *getVertexList() const { return vertexList; };
+  NodeList<Edge *> *getEdgeList() const { return edgeList; };
+
   void minimumCostSpanningTree() const;
+
 private:
   NodeList<Vertex *> *vertexList;
   NodeList<Edge *> *edgeList;
+
+  bool hasCycle() const {
+    std::unordered_map<Vertex *, bool> visited;
+    std::unordered_map<Vertex *, bool> pathVisited;
+
+    Node<Vertex *> *tmp = vertexList->getHead();
+    while (tmp) {
+      visited[tmp->getData()] = false;
+      pathVisited[tmp->getData()] = false;
+      tmp = tmp->getNext();
+    }
+
+    Node<Vertex *> *currentVertex = vertexList->getHead();
+    while (currentVertex) {
+      if (!visited[currentVertex->getData()] &&
+          dfsCheckCycle(currentVertex, visited, pathVisited))
+        return true;
+
+      currentVertex = currentVertex->getNext();
+    }
+
+    return false;
+  };
+
+  bool dfsCheckCycle(Node<Vertex *> *v,
+                     std::unordered_map<Vertex *, bool> &visited,
+                     std::unordered_map<Vertex *, bool> &pathVisited) const {
+    std::stack<Node<Vertex *> *> path;
+    Node<Edge *> *currentEdge = nullptr;
+    Node<Vertex *> *currentVertex = nullptr;
+    Vertex *otherEnd = nullptr;
+    bool hasReachedDepth = false;
+
+    path.push(v);
+    pathVisited[v->getData()] = true;
+    while (!path.empty()) {
+      currentVertex = path.top();
+      visited[currentVertex->getData()] = true;
+
+      currentEdge = currentVertex->getData()->getConnectedEdges()->getHead();
+      hasReachedDepth = true;
+
+      while (currentEdge) {
+        otherEnd =
+            currentEdge->getData()->getAnotherEnd(currentVertex->getData());
+
+        if (visited[otherEnd] && pathVisited[otherEnd])
+          return true;
+        else if (!visited[otherEnd]) {
+          path.push(vertexList->exists(otherEnd));
+          pathVisited[otherEnd] = true;
+          hasReachedDepth = false;
+        }
+
+        currentEdge = currentEdge->getNext();
+      }
+
+      // Done exploring all the adjacent nodes
+      if (hasReachedDepth) {
+        path.pop();
+        pathVisited[currentVertex->getData()] = false;
+      }
+    }
+
+    return false;
+  };
 };
 
-void Graph::minimumCostSpanningTree() const {}
+void Graph::minimumCostSpanningTree() const {
+  // Using Kruskal's algorithm and adjacent matrix
+  Graph minSpanTree;
+  Graph *tmpSpanTree;
+  Node<Vertex *> *currentVertex;
+  Node<Edge *> *currentEdge;
+
+  int **adjMatrix = nullptr;
+  getAdjacentMatrix(adjMatrix);
+
+  // NodeList<Edge*> tmpEdges(this->edgeList);
+  Edge *minEdge = nullptr;
+  int uIndex, vIndex, i, j, minWeight, nVertices = vertexList->getSize();
+  while (true) {
+    // Find the min weight in the adjacent matrix and the corresponding edge
+    // printAdjacentMatrix(&adjMatrix);
+    minWeight = INT_MAX;
+    uIndex = vIndex = -1;
+    for (i = 0; i < nVertices; i++) {
+      for (j = 0; j < nVertices; j++) {
+        if (adjMatrix[i][j] < minWeight) {
+          minWeight = adjMatrix[i][j];
+          uIndex = i;
+          vIndex = j;
+        }
+      }
+    }
+
+    if (minWeight == INT_MAX)
+      break;
+
+    minEdge = getEdge((*vertexList)[uIndex]->getData()->getData(),
+                      (*vertexList)[vIndex]->getData()->getData(), minWeight);
+    if (!minEdge)
+      break;
+
+    // Save the current spanning tree, and check if a cycle is generated when
+    // adding the new edge
+    tmpSpanTree = new Graph();
+    currentVertex = minSpanTree.getVertexList()->getHead();
+    while (currentVertex) {
+      tmpSpanTree->insertVertex(currentVertex->getData()->getData());
+      currentVertex = currentVertex->getNext();
+    }
+
+    currentEdge = minSpanTree.getEdgeList()->getHead();
+    while (currentEdge) {
+      tmpSpanTree->insertEdge((*currentEdge->getData())[0]->getData(),
+                              (*currentEdge->getData())[1]->getData(),
+                              currentEdge->getData()->getWeight());
+      currentEdge = currentEdge->getNext();
+    }
+
+    tmpSpanTree->insertVertex((*minEdge)[0]->getData());
+    tmpSpanTree->insertVertex((*minEdge)[1]->getData());
+    tmpSpanTree->insertEdge((*minEdge)[0]->getData(), (*minEdge)[1]->getData(),
+                            minEdge->getWeight());
+
+    if (!tmpSpanTree->hasCycle()) {
+      minSpanTree.insertVertex((*minEdge)[0]->getData());
+      minSpanTree.insertVertex((*minEdge)[1]->getData());
+      minSpanTree.insertEdge((*minEdge)[0]->getData(), (*minEdge)[1]->getData(),
+                             minEdge->getWeight());
+    } else
+      delete tmpSpanTree;
+
+    adjMatrix[uIndex][vIndex] = INT_MAX;
+  }
+
+  // Display the result spanning tree using graph representation
+  currentEdge = minSpanTree.getEdgeList()->getHead();
+  while (currentEdge) {
+    std::cout << currentEdge << std::endl;
+    currentEdge = currentEdge->getNext();
+  }
+
+  delete tmpSpanTree;
+}
+
+void test(Graph *g);
 
 int main() {
-  int n, m, i, u, v, w;
   Graph g;
 
+  // test(&g);
+
+  int n, m, i, u, v, w;
   std::cin >> n >> m;
 
   for (i = 0; i < n; i++) {
@@ -367,23 +590,37 @@ int main() {
 
   std::cout << std::endl;
 
-  // Generate and print the adjacency matrix
-  int **adjMatrix = nullptr;
-  g.getAdjacentMatrix(adjMatrix);
-
-  for (u = 0; u < n; u++) {
-    for (v = 0; v < n; v++, std::cout << " ")
-      std::cout << adjMatrix[u][v];
-    std::cout << std::endl;
-  }
-
-  // Clean up
-  for (u = 0; u < n; u++)
-    delete[] adjMatrix[u];
-  delete[] adjMatrix;
-
+  // g.printAdjacentMatrix();
   g.minimumCostSpanningTree();
 
   return 0;
 }
 
+void test(Graph *g) {
+  int n, m, i, u, v, w;
+  std::string filename;
+  std::cout << "Test filename: ";
+  std::getline(std::cin, filename);
+  std::ifstream inFile(filename);
+  if (!inFile) {
+    std::cout << "File could not be opened" << std::endl;
+    exit(-1);
+  }
+
+  inFile >> n >> m;
+
+  for (i = 0; i < n; i++) {
+    inFile >> u;
+    g->insertVertex(u);
+  }
+
+  for (i = 0; i < m; i++) {
+    inFile >> u >> v >> w;
+    g->insertEdge(u, v, w);
+  }
+  std::cout << std::endl;
+
+  g->printAdjacentMatrix();
+  std::cout << std::endl;
+  g->minimumCostSpanningTree();
+};
